112a55de5fc5e33f3a0f389050968a409cb899ea
Coen de Wit <coen@tcr-it.nl>
2025-08-17 01:24:21 +0200

    Add cli tools

--- apps/terminal-tools.sh
+++ apps/terminal-tools.sh
@@ -9,3 +9,16 @@ install_if_not_found ripgrep
 install_if_not_found bat
 install_if_not_found unzip
 install_if_not_found fastfetch
+install_if_not_found wget
+install_if_not_found curl
+install_if_not_found inetutils
+install_if_not_found jq
+install_if_not_found fd
+install_if_not_found eza
+install_if_not_found fzf
+install_if_not_found bat
+install_if_not_found wl-clipboard
+install_if_not_found btop
+install_if_not_found tldr
+install_if_not_found less
+install_if_not_found whois
