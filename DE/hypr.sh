dc993f53e8b18c7cd3e82e517814c5fda54c4761
Coen de Wit <coen@tcr-it.nl>
2025-08-17 01:31:30 +0200

    Hyprland

---
+++ DE/hypr.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "=== Install and configure Hyprland ==="
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+source "$SCRIPT_DIR/../helper_functions.sh"
+
+install_if_not_found hyprland
+install_if_not_found hyprshot
+install_if_not_found hyprpicker
+install_if_not_found hyprlock
+install_if_not_found hypridle
+install_if_not_found hyprsunset
+install_if_not_found polkit-gnome
+install_if_not_found hyprland-qtutils
+install_if_not_found walker-bin
+install_if_not_found libqalculate
+install_if_not_found waybar
+install_if_not_found mako
+install_if_not_found swaybg
+install_if_not_found swayosd
+install_if_not_found xdg-desktop-portal-hyprland
+install_if_not_found xdg-desktop-portal-gtk
+
+create_symlink "$SCRIPT_DIR/hypr/hyprland.conf" ~/.config/hyprland.conf
