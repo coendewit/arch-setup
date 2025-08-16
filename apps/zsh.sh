112d1b1281569f6f1afe524e75c813cc71f04dbd
Coen de Wit <coen@tcr-it.nl>
2025-08-17 00:47:59 +0200

    Add zsh

---
+++ apps/zsh.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "=== Install and configure ZSH ==="
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+source "$SCRIPT_DIR/../helper_functions.sh"
+
+install_if_not_found zsh
+
+create_symlink "$SCRIPT_DIR/zsh/.zshrc" ~/.zshrc
+
+# Change shell to zsh
+sudo chsh -s /bin/zsh
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+# Install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+# Install zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/custom/plugins/zsh-history-substring-search
+
+# Install zsh-completions
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
