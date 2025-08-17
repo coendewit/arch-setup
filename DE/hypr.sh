ecabbf2baffff584915141b9c525fbc4bdb9a859
Coen de Wit <coen@tcr-it.nl>
2025-08-17 22:47:05 +0200

    Remove walker, only in AUR

--- DE/hypr.sh
+++ DE/hypr.sh
@@ -15,7 +15,6 @@ install_if_not_found hypridle
 install_if_not_found hyprsunset
 install_if_not_found polkit-gnome
 install_if_not_found hyprland-qtutils
-install_if_not_found walker-bin
 install_if_not_found libqalculate
 install_if_not_found waybar
 install_if_not_found mako
