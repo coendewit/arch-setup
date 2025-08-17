6773d337ec126c8bf502e4afe1a005a11a453915
Coen de Wit <coen@tcr-it.nl>
2025-08-17 21:21:49 +0200

    Add auto-login

---
+++ configs/login.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Login directly as user, rely on disk encryption + hyprlock for security
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
+sudo tee /etc/systemd/system/getty@tty1.service.d/override.conf >/dev/null <<EOF
+[Service]
+ExecStart=
+ExecStart=-/usr/bin/agetty --autologin $USER --noclear %I \$TERM
+EOF
