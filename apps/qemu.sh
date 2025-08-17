23c263d4a92d501095f54d93c5cd584271acce5e
Coen de Wit <coen@tcr-it.nl>
2025-08-17 23:03:08 +0200

    Idempotency for qemu

--- apps/qemu.sh
+++ apps/qemu.sh
@@ -1,4 +1,19 @@
-sudo pacman -Syu qemu-full libvirt virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat iptables-nft
+#!/bin/bash
+
+echo "=== Install and configure QEMU ==="
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+source "$SCRIPT_DIR/../helper_functions.sh"
+
+install_if_not_found qemu-full
+install_if_not_found libvirt
+install_if_not_found virt-manager
+install_if_not_found virt-viewer
+install_if_not_found dnsmasq
+install_if_not_found vde2
+install_if_not_found bridge-utils
+install_if_not_found openbsd-netcat
+install_if_not_found iptables-nft
 
 sudo systemctl enable --now libvirtd.service
 
