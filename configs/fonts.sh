4833b64edac7d735ecf8ae952dd6ee62905d3dc7
Coen de Wit <coen@tcr-it.nl>
2025-08-18 19:12:16 +0200

    Nerfonts

--- configs/fonts.sh
+++ configs/fonts.sh
@@ -8,7 +8,9 @@ source "$SCRIPT_DIR/../helper_functions.sh"
 mkdir -p ~/.local/share/fonts
 
 if [[ ! -f ~/.local/share/fonts/JetBrainsMono-Regular.ttf ]]; then
-  cp -f "$SCRIPT_DIR/JetBrainsFonts/*.ttf" ~/.local/share/fonts
+  cp -f "$SCRIPT_DIR/fonts/*.ttf" ~/.local/share/fonts
 fi
 
 fc-cache -fv
+
+create-symlink "$SCRIPT_DIR/fonts/fonts.config" ~/.config/fontconfig/fonts.conf
