e449b0d6c9849552babae08cb1d912cc58968d73
Coen de Wit <coen@tcr-it.nl>
2025-08-18 21:36:10 +0200

    Using Rofi Wayland

--- configs/rofi.sh
+++ configs/rofi.sh
@@ -5,6 +5,6 @@ echo "=== Configure rofi ==="
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-install_if_not_found wofi
+install_if_not_found rofi-wayland
 
-cp -r "$SCRIPT_DIR/wofi" ~/.config/wofi
+cp -r "$SCRIPT_DIR/wofi" ~/.config/rofi
