47433b7284013652105305de20da1c75e46db01a
Coen de Wit <coen@tcr-it.nl>
2025-08-18 19:50:26 +0200

    Fix directories

--- DE/hyprpaper.sh
+++ DE/hyprpaper.sh
@@ -7,8 +7,8 @@ source "$SCRIPT_DIR/../helper_functions.sh"
 
 install_if_not_found hyprpaper
 
-create_symlink "$SCRIPT_DIR/hyprpaper/hyprpaper.conf" ~/.config/hyprpaper/hyprpaper.conf
+create_symlink "$SCRIPT_DIR/hyprpaper/hyprpaper.conf" ~/.config/hypr/hyprpaper.conf
 
-create_symlink "$SCRIPT_DIR/hyprpaper/osaka-jade-bg.jpg" ~/.config/hyprpaper/backgrounds/osaka-jade-bg.jpg
-create_symlink "$SCRIPT_DIR/hyprpaper/osaka-jade-bg-2.jpg" ~/.config/hyprpaper/backgrounds/osaka-jade-bg-2.jpg
-create_symlink "$SCRIPT_DIR/hyprpaper/osaka-jade-bg-3.jpg" ~/.config/hyprpaper/backgrounds/osaka-jade-bg-3.jpg
+create_symlink "$SCRIPT_DIR/hyprpaper/osaka-jade-bg.jpg" ~/.config/theme/backgrounds/osaka-jade-bg.jpg
+create_symlink "$SCRIPT_DIR/hyprpaper/osaka-jade-bg-2.jpg" ~/.config/theme/backgrounds/osaka-jade-bg-2.jpg
+create_symlink "$SCRIPT_DIR/hyprpaper/osaka-jade-bg-3.jpg" ~/.config/theme/backgrounds/osaka-jade-bg-3.jpg
