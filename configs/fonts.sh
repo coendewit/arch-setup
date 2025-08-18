151325da6c3b787d33ec4cab689bacfb9860f79b
Coen de Wit <coen@tcr-it.nl>
2025-08-18 18:59:54 +0200

    Copy font to local user

--- configs/fonts.sh
+++ configs/fonts.sh
@@ -5,8 +5,10 @@ echo "=== Install and configure JetBrains Mono ==="
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 source "$SCRIPT_DIR/../helper_functions.sh"
 
-mkdir -p /usr/share/fonts/TTF
+mkdir -p ~/.local/share/fonts
 
-if [[ ! -f /usr/share/fonts/TTF/JetBrainsMono-Regular.ttf ]]; then
-  cp -f "$SCRIPT_DIR/JetBrainsFonts/" /usr/share/fonts/TTF/
+if [[ ! -f ~/.local/share/fonts/JetBrainsMono-Regular.ttf ]]; then
+  cp -f "$SCRIPT_DIR/JetBrainsFonts/*.ttf" ~/.local/share/fonts
 fi
+
+fc-cache -fv
