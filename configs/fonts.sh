15f4318050608c33450a06574e8e436e4d286c90
Coen de Wit <coen@tcr-it.nl>
2025-08-18 19:27:36 +0200

    Fix globbing

--- configs/fonts.sh
+++ configs/fonts.sh
@@ -8,9 +8,9 @@ source "$SCRIPT_DIR/../helper_functions.sh"
 mkdir -p ~/.local/share/fonts
 
 if [[ ! -f ~/.local/share/fonts/JetBrainsMonoNerdFontMono-Regular.ttf ]]; then
-  cp -f "$SCRIPT_DIR/fonts/*.ttf" ~/.local/share/fonts
+  cp -f "$SCRIPT_DIR/fonts/"*.ttf ~/.local/share/fonts
 fi
 
 fc-cache -fv
 
-create_symlink "$SCRIPT_DIR/fonts/fonts.config" ~/.config/fontconfig/fonts.conf
+create_symlink "$SCRIPT_DIR"/fonts/fonts.config ~/.config/fontconfig/fonts.conf
