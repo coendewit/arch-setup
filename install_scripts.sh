95b0fc0e86da4fe9ed6b91fcf0379fc9708b9588
Coen de Wit <coen@tcr-it.nl>
2025-08-19 17:35:02 +0200

    Fix type

--- install_scripts.sh
+++ install_scripts.sh
@@ -3,5 +3,5 @@
 mkdir -p ~/.local/bin
 for script in ./bin/*.sh; do
   sudo cp "$script" ~/.local/bin/
-  sudo chmod +x ~/local/bin/$(basename "$script")
+  sudo chmod +x ~/.local/bin/$(basename "$script")
 done
