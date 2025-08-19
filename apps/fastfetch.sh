77247d7700bab0a0af484124212cebc284afd9c3
Coen de Wit <coen@tcr-it.nl>
2025-08-19 19:40:52 +0200

    Add required imagemagick for displaying icons

--- apps/fastfetch.sh
+++ apps/fastfetch.sh
@@ -5,6 +5,7 @@ echo "=== Install and configure Fastfetch ==="
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 source "$SCRIPT_DIR/../helper_functions.sh"
 
+install_if_not_found imagemagick
 install_if_not_found fastfetch
 
 create_symlink "$SCRIPT_DIR/fastfetch/config.jsonc" ~/.config/fastfetch/config.jsonc
