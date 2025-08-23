6a162d0c5e05a00c1757100cbc02df2e92b7cf77
Coen de Wit <coen@tcr-it.nl>
2025-08-23 15:26:27 +0200

    Fix update-monitor script

--- bin/update-monitor-setup.sh
+++ bin/update-monitor-setup.sh
@@ -5,9 +5,11 @@ EXTERNAL_MONITOR=$(hyprctl monitors | grep "DP-2" | grep -o "DP-2")
 
 if [ -n "$EXTERNAL_MONITOR" ]; then
   # External monitor is connected
-  rm -f /.config/hypr/workspaces.conf
-  ln -sf /.config/hypr/workspaces-desktop.conf /.config/hypr/workspaces.conf
+  rm -f ~/.config/hypr/workspaces.conf
+  ln -sf ~/.config/hypr/workspaces-desktop.conf ~/.config/hypr/workspaces.conf
+  echo "External monitor is connected. Using desktop workspaces."
 else
-  rm -f /.config/hypr/workspaces.conf
-  ln -sf /.config/hypr/workspaces-laptop.conf /.config/hypr/workspaces.conf
+  rm -f ~/.config/hypr/workspaces.conf
+  ln -sf ~/.config/hypr/workspaces-laptop.conf ~/.config/hypr/workspaces.conf
+  echo "External monitor is not connected. Using laptop workspaces."
 fi
