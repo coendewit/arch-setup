ad66f921141ab82c4ad586fb1bc105770b4615e9
Coen de Wit <coen@tcr-it.nl>
2025-12-21 13:21:34 +0100

    Use omarchy installation and setup

--- terminal/neovim.sh
+++ terminal/neovim.sh
@@ -6,16 +6,14 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 source "$SCRIPT_DIR/../helper_functions.sh"
 
-install_if_not_found neovim
-
-if [[ ! -f ~/.config/nvim/lazyvim.json ]]; then
-  rm -rf ~/.config/nvim
-  git clone https://github.com/LazyVim/starter ~/.config/nvim
-
-  rm -rf ~/.config/nvim/.git
-fi
-
+# install_if_not_found neovim
+#
+# if [[ ! -f ~/.config/nvim/lazyvim.json ]]; then
+#   rm -rf ~/.config/nvim
+#   git clone https://github.com/LazyVim/starter ~/.config/nvim
+#
+#   rm -rf ~/.config/nvim/.git
+# fi
+#
 create_symlink "$SCRIPT_DIR/neovim/keymaps.lua" ~/.config/nvim/lua/config/keymaps.lua
 create_symlink "$SCRIPT_DIR/neovim/supermaven.lua" ~/.config/nvim/lua/plugins/supermaven.lua
-create_symlink "$SCRIPT_DIR/neovim/theme.lua" ~/.config/nvim/lua/plugins/theme.lua
-create_symlink "$SCRIPT_DIR/neovim/transparency.lua" ~/.config/nvim/plugin/after/transparency.lua
