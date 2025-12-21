00654b092777bfd48a161060abfc30ed4428125a
Coen de Wit <coen@tcr-it.nl>
2025-12-21 13:55:41 +0100

    Remove Kitty

--- omarchy-overrides/omarchy-overrides.sh
+++ omarchy-overrides/omarchy-overrides.sh
@@ -8,9 +8,13 @@ source "$SCRIPT_DIR/../helper_functions.sh"
 create_symlink "$SCRIPT_DIR/bindings.conf" ~/.config/hypr/bindings.conf
 create_symlink "$SCRIPT_DIR/input.conf" ~/.config/hypr/input.conf
 
-source "$SCRIPT_DIR/../terminal/kitty.sh"
 source "$SCRIPT_DIR/../terminal/neovim.sh"
-source "$SCRIPT_DIR/../terminal/jetbrains-toolbox.sh"
+source "$SCRIPT_DIR/../terminal/ssh-keys.sh"
+source "$SCRIPT_DIR/../development/jetbrains-toolbox.sh"
+source "$SCRIPT_DIR/../development/dotnet.sh"
+source "$SCRIPT_DIR/../development/dev-cert.sh"
+source "$SCRIPT_DIR/../development/configure-spatialite.sh"
+source "$SCRIPT_DIR/../terminal/zsh.sh"
 
 omarchy-webapp-install "Teams" http://teams.microsoft.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/microsoft-teams.png
 omarchy-webapp-install "VGT Board" https://dev.azure.com/databalk/Debatic/_boards/board/t/1.1%20Vastgoedtabel/Backlog%20items?System.AssignedTo=%40me https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/azure-devops.png
